44aebf85cb7f5ff70e46121af40c8cb048409c4a
Nataliadiez <natalia.diez10@hotmail.com>
2021-09-07 21:48:08 -0300

    cambios en carousel del inicio

---
+++ js/bannerInicio.js
@@ -0,0 +1,21 @@
+const img_banner = document.querySelector("#img_banner")
+const title_api = document.querySelector("#title_api")
+const link_api = document.querySelector("#link_api")
+
+let datos_banner = new XMLHttpRequest;
+const url = "http://demo2420474.mockable.io/getHomeBanner"
+datos_banner.open("GET",url);
+datos_banner.send();
+
+datos_banner.onreadystatechange = (e) => {
+    if(datos_banner.readyState === 4) {
+        datos_banner = JSON.parse(datos_banner.responseText)
+        console.log(datos_banner)
+        img_banner.src = datos_banner.imgUrl
+        title_api.innerHTML = datos_banner.title
+        link_api.innerHTML = datos_banner.link
+        link_api.href = datos_banner.link
+        
+    }
+}
+
